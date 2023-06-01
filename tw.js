7cab45bedbb8e4c52a0d67fa819014c86e3fe7d0
GVulpoi <gvulpoi@yahoo.com>
2023-06-01 05:11:47 +0300

    js1

---
+++ tw.js
@@ -0,0 +1,79 @@
+window.onload = function() { 
+
+	q();
+	function q () {
+		//q1
+
+	
+	document.getElementById('a1').addEventListener("click" , function () {
+		document.getElementById('q1').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a2').addEventListener("click" , function () {
+		document.getElementById('q1').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a3').addEventListener("click" , function () {
+		document.getElementById('q1').style.color ="#408E0E";
+	});
+	
+	//q2
+
+	document.getElementById('a4').addEventListener("click" , function () {
+		document.getElementById('q2').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a5').addEventListener("click" , function () {
+		document.getElementById('q2').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a6').addEventListener("click" , function () {
+		document.getElementById('q2').style.color ="#408E0E";
+	});
+
+	//q3
+
+	document.getElementById('a7').addEventListener("click" , function () {
+		document.getElementById('q3').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a8').addEventListener("click" , function () {
+		document.getElementById('q3').style.color ="#408E0E";
+	});
+
+	document.getElementById('a9').addEventListener("click" , function () {
+		document.getElementById('q3').style.color ="#BA0C0C";
+	});
+
+	//q4
+
+	document.getElementById('a10').addEventListener("click" , function () {
+		document.getElementById('q4').style.color ="#408E0E";
+	});
+
+	document.getElementById('a11').addEventListener("click" , function () {
+		document.getElementById('q4').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a12').addEventListener("click" , function () {
+		document.getElementById('q4').style.color ="#BA0C0C";
+	});
+
+	//q5
+
+	document.getElementById('a13').addEventListener("click" , function () {
+		document.getElementById('q5').style.color ="#408E0E";
+	});
+
+	document.getElementById('a14').addEventListener("click" , function () {
+		document.getElementById('q5').style.color ="#BA0C0C";
+	});
+
+	document.getElementById('a15').addEventListener("click" , function () {
+		document.getElementById('q5').style.color ="#BA0C0C";
+	});
+
+	}
+}
+
+
